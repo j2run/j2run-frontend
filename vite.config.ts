67c4faef600051f6670869f78d63f2bab492fa60
wawahuy <kakahuy99@gmail.com>
2024-01-28 14:01:33 +0700

    Fix split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,24 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         output: {
           manualChunks(id: string) {
-            const rg = /node_modules\/(?<name>([^\/]*))\/(?<nameChild>([^\/]*))\//.exec(id);
-            if (rg && rg.groups) {
-              let name = rg.groups.name;
-              if (name.startsWith('@')) {
-                name += '-' + rg.groups.nameChild;
-              }
-              return 'vendor-' + name;
+            // const rg = /node_modules\/(?<name>([^\/]*))\/(?<nameChild>([^\/]*))\//.exec(id);
+            // if (rg && rg.groups) {
+            //   let name = rg.groups.name;
+            //   if (name.startsWith('@')) {
+            //     name += '-' + rg.groups.nameChild;
+            //   }
+            //   return 'vendor-' + name;
+            // }
+            if (id.includes('@vuelidate')) {
+              return 'vendor-vuelidate';
+            } else if (id.includes('@vueuse')) {
+              return 'vendor-vueuse';
+            } else if (id.includes('vuetify')) {
+              return 'vendor-vuetify';
+            } else if (id.includes('@novnc')) {
+              return 'vendor-novnc';
+            } else if (id.includes('@vue')) {
+              return 'vendor-vue';
             }
           },
         },
